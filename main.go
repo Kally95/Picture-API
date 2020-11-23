28b5d26ac42f3d7ae1c5864733e90e3393622012
Kally95 <kally-95@hotmail.com>
2020-11-23 15:55:17 +0000

    Checks whether file exists. Returns 200 or 400 accordingly

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"os"
 	"strings"
 )
 
@@ -13,19 +14,30 @@ func check(e error) {
 	}
 }
 
+const path = "/home/santito/Desktop/PictureAPI/images/"
+
 func writeImage(w http.ResponseWriter, r *http.Request) {
 	d, err := ioutil.ReadAll(r.Body)
 	defer r.Body.Close()
 	check(err)
 	animal := string(d)
-	image := animalPicture(animal)
-	fmt.Print(image)
-	w.Write(image)
+	image := retrievePicture(animal)
+	if image != nil {
+		w.Header().Set("Content-Type", "image/jpeg")
+		w.WriteHeader(200)
+		w.Write(image)
+	} else {
+		w.WriteHeader(400)
+		fmt.Fprintf(w, `Sorry! We don't have the image you're looking for. Try "Cat"`)
+	}
 }
 
-func animalPicture(a string) []byte {
+func retrievePicture(a string) []byte {
 	str := strings.Title(strings.ToLower(a))
-	image, err := ioutil.ReadFile("/home/santito/Desktop/PictureAPI/images/" + str + ".jpeg")
+	if _, err := os.Stat(path + str + ".jpeg"); os.IsNotExist(err) {
+		return nil
+	}
+	image, err := ioutil.ReadFile(path + str + ".jpeg")
 	check(err)
 	return image
 }
@@ -34,3 +46,6 @@ func main() {
 	http.HandleFunc("/", writeImage)
 	http.ListenAndServe(":3000", nil)
 }
+
+// If image isnt available, custom error errors.New("can't work with 42")
+// and a 400 (bad request) status code.
