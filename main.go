148d26ee9c01cdbcc729b39286b7b437cb981ce9
Kally95 <kally-95@hotmail.com>
2020-12-05 12:45:55 +0000

    .

--- main.go
+++ main.go
@@ -51,3 +51,7 @@ func main() {
 	http.HandleFunc("/", WriteImage)
 	http.ListenAndServe(":3000", nil)
 }
+
+// Consider a funcion that returns a slice of strings containing
+// the names of all images, loop through that using the input value
+// and extension variable to extraxt desired animal.
